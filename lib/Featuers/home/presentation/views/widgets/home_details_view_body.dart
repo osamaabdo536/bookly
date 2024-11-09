e59753c2dad44f308e8c408040b7defb1e3b9e92
osamaabdo536 <93677924+osamaabdo536@users.noreply.github.com>
2024-11-10 00:35:34 +0200

    refactor book details view into sections.

--- lib/Featuers/home/presentation/views/widgets/home_details_view_body.dart
+++ lib/Featuers/home/presentation/views/widgets/home_details_view_body.dart
@@ -1,74 +1,28 @@
-import 'package:bookly/Featuers/home/presentation/views/widgets/book_rating.dart';
-import 'package:bookly/Featuers/home/presentation/views/widgets/custom_book_image.dart';
-import 'package:bookly/Featuers/home/presentation/views/widgets/similar_books_list_view.dart';
-import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/Featuers/home/presentation/views/widgets/similar_book_section.dart';
 import 'package:flutter/material.dart';
-import 'books_action.dart';
-import 'custom_book_details_app_bar.dart';
+import 'book_details_section.dart';
 
 class HomeDetailsViewBody extends StatelessWidget {
   const HomeDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
 
-    return CustomScrollView(
+    return const CustomScrollView(
       slivers: [
         SliverFillRemaining(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
+            padding: EdgeInsets.symmetric(horizontal: 30),
             child: Column(
               children: [
-                const CustomBookDetailsAppBar(),
-                SizedBox(
-                  width: width * 0.48,
-                  child: const CustomBookImage(),
-                ),
-                const SizedBox(
-                  height: 46,
-                ),
-                const Text(
-                  'The Jungle Book',
-                  style: Styles.textStyle30,
-                ),
-                const SizedBox(
-                  height: 6,
-                ),
-                Opacity(
-                  opacity: 0.7,
-                  child: Text(
-                    'Rudyard Kipling',
-                    style: Styles.textStyle18.copyWith(fontStyle: FontStyle.italic),
-                  ),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                const BookRating(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                ),
-                const SizedBox(
-                  height: 36,
-                ),
-                const BooksAction(),
-                const Expanded(
+                BookDetailsSection(),
+                Expanded(
                   child: SizedBox(
                     height: 50,
                   ),
                 ),
-                const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'You can also like',
-                    style: Styles.textStyle16,
-                  ),
-                ),
-                const SizedBox(
-                  height: 14,
-                ),
-                const SimilarBooksListView(),
-                const SizedBox(
+                SimilarBooksSection(),
+                SizedBox(
                   height: 40,
                 ),
               ],
---
+++ lib/Featuers/home/presentation/views/widgets/similar_book_section.dart
@@ -0,0 +1,24 @@
+import 'package:bookly/Featuers/home/presentation/views/widgets/similar_books_list_view.dart';
+import 'package:flutter/material.dart';
+import '../../../../../core/utils/styles.dart';
+
+class SimilarBooksSection extends StatelessWidget {
+  const SimilarBooksSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'You can also like',
+          style: Styles.textStyle16,
+        ),
+        SizedBox(
+          height: 14,
+        ),
+        SimilarBooksListView(),
+      ],
+    );
+  }
+}
