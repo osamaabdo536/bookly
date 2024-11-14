68a7c5539da260838fc2ee4ccc85a174bf37cd23
osamaabdo536 <93677924+osamaabdo536@users.noreply.github.com>
2024-11-14 12:06:06 +0200

    create book entity

---
+++ lib/Featuers/home/domain/book_entity.dart
@@ -0,0 +1,15 @@
+class BookEntity {
+  final String image;
+  final String authorName;
+  final String title;
+  final num price;
+  final num rating;
+
+  BookEntity({
+    required this.image,
+    required this.authorName,
+    required this.title,
+    required this.price,
+    required this.rating,
+  });
+}
