99983a9620556543f20f9023ef87cd6bbc59c4c3
osamaabdo536 <93677924+osamaabdo536@users.noreply.github.com>
2024-11-16 12:25:50 +0200

    refactor fetch featured books use case

--- lib/Featuers/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/Featuers/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -2,14 +2,16 @@ import 'package:bookly/Featuers/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/errors/failure.dart';
+import '../../../../core/use_case/use_case.dart';
 import '../entities/book_entity.dart';
 
-class FetchFeaturedBooksUseCase {
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity> , Noparam> {
   final HomeRepo homeRepo;
 
   FetchFeaturedBooksUseCase({required this.homeRepo});
 
-  Future<Either<Failure,List<BookEntity>>> fetchFeaturedBooks(){
-    return homeRepo.fetchFeaturedBooks();
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([Noparam? param]) async{
+    return await homeRepo.fetchFeaturedBooks();
   }
 }
---
+++ lib/core/use_case/use_case.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import '../errors/failure.dart';
+
+abstract class UseCase<Type , param> {
+  Future<Either<Failure ,Type>> call([param param]);
+}
+
+class Noparam {}
